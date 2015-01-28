728de9701fb35774cfb75661f90d5f9f6e44e5e0
Julian Straub <jstraub@csail.mit.edu>
2015-01-28 14:05:28 -0500

    adding the necessary files for crp and test of crp...

---
+++ include/crpMM.hpp
@@ -0,0 +1,183 @@
+/* Copyright (c) 2015, Julian Straub <jstraub@csail.mit.edu>
+ * Licensed under the MIT license. See the license file LICENSE.
+ */
+#pragma once
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+#include <Eigen/Dense>
+
+#include <boost/shared_ptr.hpp>
+
+#include "dpMM.hpp"
+#include "cat.hpp"
+#include "dir.hpp"
+#include "niw.hpp"
+#include "sampler.hpp"
+#include "basemeasure.hpp"
+
+using namespace Eigen;
+using std::endl; using std::cout;
+using boost::shared_ptr;
+
+  
+  // algorithm 2 of Neal 
+  // http://www.stat.columbia.edu/npbayes/papers/neal_sampling.pdf
+template<typename T>
+class CrpMM : public DpMM<T>
+{
+public:
+  CrpMM(const T alpha, const shared_ptr<BaseMeasure<T> >& theta, uint32_t K0,
+      boost::mt19937* pRndGen);
+  virtual ~CrpMM()
+  {};
+
+  virtual void initialize(const shared_ptr<Matrix<T,Dynamic,Dynamic> >& spx);
+  virtual void initialize(const Matrix<T,Dynamic,Dynamic>& x)
+  {this->spx_=shared_ptr<Matrix<T,Dynamic,Dynamic> >(new
+      Matrix<T,Dynamic,Dynamic>(x)); initialize(this->spx_);};
+  virtual void sampleLabels();
+  virtual void sampleParameters();
+  virtual void proposeSplits() {};
+  virtual void proposeMerges() {};
+  // call this right before sampleParameters()
+  virtual void resampleFromBase(uint32_t Kmax) {};
+  virtual const VectorXu & getLabels(){return z_;};
+  virtual const VectorXu & labels(){return z_;};
+  virtual Matrix<T,Dynamic,1> getCounts();
+  virtual uint32_t getK() const { return K_;};
+  virtual double logJoint() { return 0.0;}; 
+
+//  virtual MatrixXu mostLikelyInds(uint32_t n) 
+//{ return MatrixXu::Zero(n,1);};
+  virtual MatrixXu mostLikelyInds(uint32_t n, Matrix<T,Dynamic,Dynamic>& logLikes)
+  { return MatrixXu::Zero(n,1);};
+
+private:
+  void removeEmptyClusters();
+
+  uint32_t K_;
+  T alpha_; // dp concentration paramter
+
+  shared_ptr<BaseMeasure<T> > theta0_;
+  vector<shared_ptr<BaseMeasure<T> > > thetas_;
+
+  VectorXu z_;
+
+  boost::mt19937* pRndGen_;
+};
+
+// ------------------------- impl -----------------------------------
+template<typename T>
+CrpMM<T>::CrpMM(const T alpha, const shared_ptr<BaseMeasure<T> >& theta,
+    uint32_t K0,  boost::mt19937* pRndGen)
+  : K_(K0), alpha_(alpha), theta0_(theta), pRndGen_(pRndGen)
+{};
+
+template <typename T>
+Matrix<T,Dynamic,1> CrpMM<T>::getCounts()
+{
+  return counts<T,uint32_t>(z_,K_);
+};
+
+template<typename T>
+void CrpMM<T>::initialize(const shared_ptr<Matrix<T,Dynamic,Dynamic> >& spx)
+{
+  cout<<"init"<<endl;
+  this->spx_ = spx;
+  // randomly init labels from prior
+  z_.setZero(spx->cols());
+
+  Matrix<T,Dynamic,1> alpha = (alpha_/K_)*Matrix<T,Dynamic,1>::Ones(K_); 
+  Cat<T> pi = Dir<Cat<T>,T>(alpha,pRndGen_).sample(); 
+  for(uint32_t i=0; i<z_.size(); ++i)
+  {
+    z_(i) = pi.sample();
+  }
+
+  // init the parameters
+  if(thetas_.size() == 0)
+  {
+    cout<<"creating thetas"<<endl;
+    for (uint32_t k=0; k<K_; ++k)
+      thetas_.push_back(shared_ptr<BaseMeasure<T> >(theta0_->copy()));
+  }
+};
+
+template<typename T>
+void CrpMM<T>::sampleLabels()
+{
+  Matrix<T,Dynamic,1> Nk(K_+1);
+  Nk.topRows(K_) = counts<T,uint32_t>(z_,K_);
+  Nk(K_) = alpha_;
+//  cout<<"Nk="<<Nk.transpose()<<" alpha="<<alpha_<<endl;
+  for(uint32_t i=0; i<z_.size(); ++i)
+  {
+    Nk(z_(i)) --; // take the data-point out of that cluster
+    Matrix<T,Dynamic,1> pdf = Nk.array().log().matrix();
+    pdf = pdf.array() - log(Nk.sum());
+//    cout<<pdf.transpose()<< " -> ";
+    // add the log posteriors of the individual clusters
+    for(uint32_t k=0; k<K_; ++k)
+      pdf(k) += thetas_[k]->logLikelihood(this->spx_->col(i)); 
+    // add the log likelihood of data under the hyperparameters
+    pdf(K_) += theta0_->logPdfUnderPriorMarginalized(this->spx_->col(i));
+//    cout<<pdf.transpose()<<" exp: ";
+    // normalize and exponentiate pdf
+    pdf = (pdf.array() - logSumExp(pdf)).exp().matrix();
+//    cout<<pdf.transpose()<<endl;
+    z_(i) = Catd(pdf,pRndGen_).sample();
+    Nk(z_(i)) ++; // add data-point into new cluster
+
+    // add a new cluster from the base measure
+    if(z_(i)==K_){
+      thetas_.push_back(shared_ptr<BaseMeasure<T> >(theta0_->copy()));
+      // TODO might want to add z_i to the SS of the new cluster?
+      thetas_[z_(i)]->posterior(*this->spx_,z_,K_); // TODO slow
+    }
+  }
+  this->removeEmptyClusters();
+};
+
+template<typename T>
+void CrpMM<T>::sampleParameters()
+{
+//#pragma omp parallel for 
+  for(uint32_t k=0; k<K_; ++k)
+  {
+    thetas_[k]->posterior(*this->spx_,z_,k);
+    cout<<"k:"<<k<<" ";
+    thetas_[k]->print();
+  }
+};
+
+template <typename T>
+void CrpMM<T>::removeEmptyClusters()
+{
+  std::vector<bool> toDelete(K_,true);
+#pragma omp parallel for 
+  for(uint32_t i=0; i<z_.size(); ++i)
+    for(uint32_t k=0; k<K_; ++k) 
+      toDelete[z_(i)] = toDelete[z_(i)] && !(z_(i)==k);
+
+  std::vector<uint32_t> labelMap(K_,0);
+  {
+    uint32_t k=0;
+    for(k=0; k<K_; ++k) if(!toDelete[k]) break;
+    for(k=k==0?1:k; k<K_; ++k)
+      if(toDelete[k-1]) 
+        labelMap[k] = labelMap[k-1];
+      else
+        labelMap[k] = labelMap[k-1]+1;
+//    for(k=0; k<K_; ++k) cout<<toDelete[k]<<" "; cout<<endl;
+//    for(k=0; k<K_; ++k) cout<<labelMap[k]<<" "; cout<<endl;
+  }
+#pragma omp parallel for
+  for(uint32_t i=0; i<z_.size(); ++i)
+    z_(i) = labelMap[z_(i)];
+
+  for(int32_t k=K_-1; k>=0; --k)
+    if (toDelete[k])
+      thetas_.erase(thetas_.begin()+k);
+  K_ = labelMap[K_-1]+1;
+}
---
+++ test/crpMM.cpp
@@ -0,0 +1,159 @@
+/* Copyright (c) 2015, Julian Straub <jstraub@csail.mit.edu>
+ * Licensed under the MIT license. See the license file LICENSE.
+ */
+
+#include <iostream>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE crpMM test
+#include <boost/test/unit_test.hpp>
+
+#include "crpMM.hpp"
+#include "niwBaseMeasure.hpp"
+#include "niwSphere.hpp"
+#include "vmfBaseMeasure.hpp"
+
+//BOOST_AUTO_TEST_CASE(crpMM_test)
+//{
+//
+//  double nu = 4.0;
+//  double kappa = 4.0;
+//  MatrixXd Delta(3,3);
+//  Delta << .1,0.0,0.0,
+//        0.0,.1,0.0,
+//        0.0,0.0,.1;
+//  VectorXd theta(3);
+//  theta << 0.0,0.0,0.0;
+//
+//  boost::mt19937 rndGen(9191);
+//  NIW<double> niw(Delta,theta,nu,kappa,&rndGen);
+//
+//  shared_ptr<NiwMarginalized<double> > niwMargBase(
+//      new NiwMarginalized<double>(niw));
+//  double alpha = 1.0;
+//  CrpMM<double> dirGMM_marg(alpha,niwMargBase,&rndGen);
+//  
+//  uint32_t N=20;
+//  MatrixXd x(3,N);
+//  for(uint32_t i=0; i<N; ++i)
+//    if(i<N/2)
+//      x.col(i) << 0.0,0.0,0.0;
+//    else
+//      x.col(i) << 10.0,10.0,10.0;
+//  dirGMM_marg.initialize(x);
+//  cout<<"------ sampling -- NIW marginalized"<<endl;
+//  cout<<dirGMM_marg.labels().transpose()<<endl;
+//  for(uint32_t t=0; t<30; ++t)
+//  {
+//    dirGMM_marg.sampleLabels();
+//    dirGMM_marg.sampleParameters();
+//    cout<<dirGMM_marg.labels().transpose()
+//      <<" logJoint="<<dirGMM_marg.logJoint()<<endl;
+//  }
+//
+//  shared_ptr<NiwSampled<double> > niwSampled(
+//      new NiwSampled<double>(niw));
+//  CrpMM<double> dirGMM_samp(alpha,niwSampled,&rndGen);
+//  
+//  dirGMM_samp.initialize(x);
+//  cout<<"------ sampling -- NIW sampled"<<endl;
+//  cout<<dirGMM_samp.labels().transpose()<<endl;
+//  for(uint32_t t=0; t<30; ++t)
+//  {
+//    dirGMM_samp.sampleLabels();
+//    dirGMM_samp.sampleParameters();
+//    cout<<dirGMM_samp.labels().transpose()
+//      <<" logJoint="<<dirGMM_samp.logJoint()<<endl;
+//  }
+//};
+//
+//
+//BOOST_AUTO_TEST_CASE(crpMM_Sphere_test)
+//{
+//  cout<<"------ sampling -- NIW sphere"<<endl;
+//
+//  double nu = 20.0;
+//  MatrixXd Delta(2,2);
+//  Delta << .01,0.0,
+//        0.0,.01;
+//  Delta *= nu;
+//
+//  boost::mt19937 rndGen(9191);
+//  IW<double> iw(Delta,nu,&rndGen);
+//  shared_ptr<NiwSphere<double> > niwSp( new NiwSphere<double>(iw,&rndGen));
+//  double alpha = 1.0;
+//  CrpMM<double> dirGMM_sp(alpha,niwSp,&rndGen);
+//  
+//  uint32_t N=20;
+//  uint32_t K=2;
+//  MatrixXd x(3,N);
+//  MatrixXd mus = sampleClustersOnSphere<double>(x, K);
+//
+//  dirGMM_sp.initialize(x);
+//
+//  cout<<"true means: "<<endl<<mus<<endl;
+//  cout<<dirGMM_sp.labels().transpose()<<endl;
+//  for(uint32_t t=0; t<10; ++t)
+//  {
+//    dirGMM_sp.sampleParameters();
+////    for(uint32_t k=0; k<dirGMM_sp.getK(); ++k)
+////    {
+////      cout<<"  k: "<<k<<" "<<endl; 
+////      dirGMM_sp.getTheta(k)->print();
+////    }
+//    dirGMM_sp.sampleLabels();
+//    cout<<"@t="<<t<<" "<<dirGMM_sp.labels().transpose()
+//      <<" logJoint="<<dirGMM_sp.logJoint()<<endl;
+//  }
+//  MatrixXd logLikes;
+//  MatrixXu inds = dirGMM_sp.mostLikelyInds(5,logLikes);
+//  cout<<"most likely indices"<<endl;
+//  cout<<inds<<endl;
+//  cout<<"----------------------------------------"<<endl;
+//};
+//
+BOOST_AUTO_TEST_CASE(crpMM_vMF_test)
+{
+  cout<<"------ sampling -- Dir-vMF"<<endl;
+
+  double a0 = 5.0;
+  double b0 = 4.7;
+  double t0 = 0.1;
+  VectorXd m0(3);
+  m0 << 1.0,0.0,0.0;
+
+  boost::mt19937 rndGen(9191);
+
+  vMFpriorFull<double> vMFprior(m0,t0,a0,b0,&rndGen);
+  shared_ptr<vMFbase<double> > vMFsampled( new vMFbase<double>(vMFprior));
+  
+  double alpha = 1.0;
+  CrpMM<double> dirvMF_sp(alpha,vMFsampled,3,&rndGen);
+  
+  uint32_t N=20;
+  uint32_t K=2;
+  MatrixXd x(3,N);
+  MatrixXd mus = sampleClustersOnSphere<double>(x, K);
+
+  dirvMF_sp.initialize(x);
+
+  cout<<"true means: "<<endl<<mus<<endl;
+  cout<<dirvMF_sp.labels().transpose()<<endl;
+  for(uint32_t t=0; t<10; ++t)
+  {
+    dirvMF_sp.sampleParameters();
+//    for(uint32_t k=0; k<dirvMF_sp.getK(); ++k)
+//    {
+//      cout<<"  k: "<<k<<" "<<endl; 
+//      dirvMF_sp.getTheta(k)->print();
+//    }
+    dirvMF_sp.sampleLabels();
+    cout<<"@t="<<t<<" "<<dirvMF_sp.labels().transpose()
+      <<" logJoint="<<dirvMF_sp.logJoint()<<endl;
+  }
+//  MatrixXd logLikes;
+//  MatrixXu inds = dirvMF_sp.mostLikelyInds(5,logLikes);
+//  cout<<"most likely indices"<<endl;
+//  cout<<inds<<endl;
+  cout<<"----------------------------------------"<<endl;
+};
+
